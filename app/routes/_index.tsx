688c41adfdfe9cfbbe3254cb48d6f6b8976350fe
revelcw <github@revelcw.com>
2023-05-14 10:08:42 -0400

    add index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { V2_MetaFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'New Remix App' }];
@@ -6,34 +7,13 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            className="text-blue-500"
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div
+      style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}
+      className="flex flex-col items-center p-16"
+    >
+      <Link to="/drills" className="bg-secondary rounded-md p-4">
+        View Drills
+      </Link>
     </div>
   );
 }
